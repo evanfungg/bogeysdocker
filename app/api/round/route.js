7fff9bb320928b0be438df545236efb249bbce48
Your Name <you@example.com>
2024-03-19 22:17:52 -0700

    sg

--- app/api/round/route.js
+++ app/api/round/route.js
@@ -8,7 +8,7 @@ export async function POST(request) {
     const { roundName } = await request.json();
     const session = await getSession(request);
 
-    // Check if session and user information is available along with roundName
+    
     if (!session || !session.user || !roundName) {
       return new NextResponse(JSON.stringify({ error: "Missing user information or round name" }), { status: 400 });
     }
@@ -17,25 +17,25 @@ export async function POST(request) {
 
     await connectToMongoDB();
 
-    // Attempt to find the user by username
+    
     let currentUser = await UserModel.findOne({ username: user.name });
 
-    // If the user does not exist, create a new one with the username and an empty rounds array
+    
     if (!currentUser) {
       currentUser = new UserModel({
-        username: user.name, // This matches the username field in your schema
-        rounds: [] // Initializes with an empty rounds array
+        username: user.name, 
+        rounds: [] 
       });
       await currentUser.save();
     }
 
-    // Adding a new round to the user's rounds array
+    
     currentUser.rounds.push({
-      name: roundName, // The name of the round, as provided in the request
-      holes: [] // Starts with an empty holes array
+      name: roundName, 
+      holes: [] 
     });
 
-    await currentUser.save(); // Save the updated user document
+    await currentUser.save(); 
 
     return new NextResponse(JSON.stringify({ message: "Round created successfully" }), { status: 201 });
   } catch (error) {
