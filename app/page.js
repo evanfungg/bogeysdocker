ff7d1f91104a091a277a95c9fe49a45fab2d7532
Your Name <you@example.com>
2024-03-19 22:31:56 -0700

    image

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="container">
         <div className="content">
           <div className="image-container">
-            <img src="/14_golf.jpg" alt="Golf Image" />
+            <img src="/14_Golf.jpg" alt="Golf Image" />
           </div>
           <div className="title-container">
             <h2 className="title">18 Bogeys</h2>
